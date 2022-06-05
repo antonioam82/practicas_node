37d91cb0d7259fdb583b53c4d54132e11aa2f96e
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2022-06-05 16:11:15 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 const {frutas, dinero} = require('./frutas.js');
+var cowsay = require("cowsay");
+
+console.log(cowsay.say({
+    text : "What's up dude!",
+    e : "oO",
+    T : "U "
+}));
 
 frutas.forEach(item => {
     console.count(item)
